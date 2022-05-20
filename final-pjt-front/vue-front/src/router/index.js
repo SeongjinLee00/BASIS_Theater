94f0fb557f6172b7b716e9c1b9829b45879c34a0
NaGyeongPark <ngp.july@gmail.com>
2022-05-20 13:39:56 +0900

    Navigation Guard Setting

--- final-pjt-front/vue-front/src/router/index.js
+++ final-pjt-front/vue-front/src/router/index.js
@@ -5,19 +5,79 @@ import HomeView from '../views/HomeView.vue'
 Vue.use(VueRouter)
 
 const routes = [
+  // accounts
   {
-    path: '/',
-    name: 'home',
-    component: HomeView
+    path: '/login',
+    name: 'login',
+    component: LoginView,
   },
   {
-    path: '/about',
-    name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
-  }
+    path: '/logout',
+    name: 'logout',
+    component: LogoutView,
+  },
+  {
+    path: '/signup',
+    name: 'signup',
+    component: SignupView,
+  },
+  {
+    path: '/profile/:username',
+    name: 'profile',
+    component: ProfileView,
+  },
+  // movies
+  {
+    path: '/', // HOME
+    name: 'movies',
+    component: MovieListView,
+  },
+  {
+    path: '/movies/new',
+    name: 'movieNew',
+    component: MovieNewView,
+  },
+  {
+    path: '/movies/:moviePk',
+    name: 'movie',
+    component: MovieDetailView,
+  },
+  {
+    path: 'movies/:moviePk/edit',
+    name: 'MovieEdit',
+    component: MovieEditView,
+  },
+  // communities
+  {
+    path: '/communities',
+    name: 'communities',
+    component: CommunityListView,
+  },
+  {
+    path: '/communities/new',
+    name: 'communityNew',
+    component: CommunityNewView,
+  },
+  {
+    path: '/communities/:communityPk',
+    name: 'community',
+    component: CommunityDetailView,
+  },
+  {
+    path: '/communities/:communityPk/edit',
+    name: 'communityEdit',
+    component: CommunityEditView,
+  },
+  // etc
+  {
+    path: '/404',
+    name: 'NotFound404',
+    component: NotFound404,
+  },
+  {
+    path: '*',
+    redirect: '/404',
+  },
 ]
 
 const router = new VueRouter({
@@ -25,5 +85,51 @@ const router = new VueRouter({
   base: process.env.BASE_URL,
   routes
 })
+/*
+Navigation Guard 설정
+  (이전 페이지에서 있던 에러 메시지 삭제)
+
+  로그인(Authentication)이 필요 없는 route 이름들 저장(/login, /signup)
+
+  0. router 에서 이동 감지
+
+  1. 현재 이동하고자 하는 페이지가 로그인이 필요한지 확인
+  
+  2. 로그인이 필요한 페이지인데 로그인이 되어있지 않다면
+    로그인 페이지(/login)로 이동
+
+  3. 로그인이 되어 있다면
+    원래 이동할 곳으로 이동
+  
+  4. 로그인이 되어있는데 /login, /signup 페이지로 이동한다면
+    메인 페이지(/)로 이동
+    
+
+*/
+
+router.beforeEach((to, from, next) => {
+  // 이전 페이지에서 발생한 에러msg 삭제
+  store.commit('SET_AUTH_ERROR', null)
+
+  const { isLoggedIn } = store.gettters
+  // 인증이 필요없는 페이지들
+  const noAuthPages = [ 'login', 'signup' ]
+
+  const isAuthRequired = !noAuthPages.includes(to.name)
+
+  if (isAuthRequired && !isLoggedIn ) {
+    alert('로그인이 필요합니다')
+    next({name: 'login'})
+  } else {
+    next()
+  }
+
+  // 로그인 되어있는데 login이나 signup으로가면 main 페이지로 이동
+  if (!isAuthRequired && !isLoggedIn) {
+    next({name: 'movies'})
+  }
+})
+
+
 
 export default router
